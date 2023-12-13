435cadeb9a0aaf74e2222528d91f6e48167c269f
inion13 <inion13@gmail.com>
2023-12-13 17:11:24 +0800

    add registration

--- InionBlog/example/views.py
+++ InionBlog/example/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.http import HttpResponse
+from django.shortcuts import render, redirect
 
 
 lst_recipes = [
@@ -19,6 +22,41 @@ lst_recipes = [
     {'id': 4, 'title': ' ', 'description': ' '},
     {'id': 5, 'title': ' ', 'description': ' '}
 ]
+
+
+def user_login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+    else:
+        form = AuthenticationForm()
+    return render(request, 'login.html', {'form': form})
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
+
+
+@login_required(login_url='login')
+def hello(request):
+    return HttpResponse('Hello, World!')
+
+
+def user_registration(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration.html', {'form': form})
 
 
 def main(request):
@@ -44,6 +82,8 @@ def recipe(request, recipe_id):
             result['description'] = i['description']
             break
     return render(request, 'recipe.html', result)
+
+
 
 
 '''def create_item(request):
